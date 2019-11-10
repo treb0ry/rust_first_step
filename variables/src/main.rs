5a55019221620caa6d8f8e016b305b8d6110a1c0
treb0ry <vinodel2589@gmai.com>
2019-11-10 19:47:23 +0300

    done variables and mutability

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+const MAX_POINTS: u32 = 100_000;
+fn main() {
+    let mut x = 5;
+    println!("Thr value of x is: {}", x);
+    x = 6;
+    println!("The value of x: {}", x);
+    println!("The value of MAX_POINTS: {}", MAX_POINTS);
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y: {}", y)
+}
