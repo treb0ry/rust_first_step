6453bd3f1d1f16c43ecd23e20730d3ea4888ea85
treb0ry <vinodel2589@gmai.com>
2019-11-21 22:53:30 +0300

    delete target folder

--- structs/target/debug/structs.d
+++
@@ -1,1 +0,0 @@
-C:\Users\vinog\OneDrive\Documents\Programing\RUST\rust_first_step\structs\target\debug\structs.exe: C:\Users\vinog\OneDrive\Documents\Programing\RUST\rust_first_step\structs\src\main.rs
