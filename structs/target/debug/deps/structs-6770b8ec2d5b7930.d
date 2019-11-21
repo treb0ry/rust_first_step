0ab6c7fbf3ce49e972b760825a89bad1a6dd704f
treb0ry <vinodel2589@gmai.com>
2019-11-21 22:22:21 +0300

    done defining structs

---
+++ structs/target/debug/deps/structs-6770b8ec2d5b7930.d
@@ -0,0 +1,5 @@
+C:\Users\vinog\OneDrive\Documents\Programing\RUST\rust_first_step\structs\target\debug\deps\structs-6770b8ec2d5b7930.exe: src\main.rs
+
+C:\Users\vinog\OneDrive\Documents\Programing\RUST\rust_first_step\structs\target\debug\deps\structs-6770b8ec2d5b7930.d: src\main.rs
+
+src\main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+C:\Users\vinog\OneDrive\Documents\Programing\RUST\rust_first_step\structs\target\debug\structs.exe: C:\Users\vinog\OneDrive\Documents\Programing\RUST\rust_first_step\structs\src\main.rs
